32d19858325047bae70fd81f515819f8e5f0acfa
aitorortunio <43756956+aitorortunio@users.noreply.github.com>
2021-06-15 17:43:17 -0300

    Agrego const source logo url y renombres Info a Card

--- src/main/java/ayds/apolo1/lastfm/artistInfo/LastFmToArtistCardResolver.kt
+++ src/main/java/ayds/apolo1/lastfm/artistInfo/LastFmToArtistCardResolver.kt
@@ -5,24 +5,28 @@ import ayds.apolo1.lastfm.entities.CardImpl
 import com.google.gson.Gson
 import com.google.gson.JsonObject
 
-interface LastFmToArtistInfoResolver {
-    fun getArtistInfoFromExternalData(serviceData: String?): CardImpl?
+interface LastFmToArtistCardResolver {
+    fun getArtistCardFromExternalData(serviceData: String?): CardImpl?
 }
 
 private const val ARTISTS = "artist"
-private const val NAME = "name"
 private const val URL = "url"
 private const val BIO = "bio"
 private const val CONTENT = "content"
+private const val SOURCE_LOGO_URL =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Lastfm_logo.svg/320px-Lastfm_logo.svg.png"
 
-internal class JsonToArtistInfoResolver() :
-    LastFmToArtistInfoResolver {
+internal class JsonToArtistCardResolver() :
+    LastFmToArtistCardResolver {
 
-    override fun getArtistInfoFromExternalData(serviceData: String?): CardImpl? =
+    override fun getArtistCardFromExternalData(serviceData: String?): CardImpl? =
         try {
             serviceData?.getFirstResult()?.let { item ->
                 CardImpl(
-                    item.getArtistName(), item.getInfoContent(), item.getFullArticleUrl()
+                    description = item.getInfoContent(),
+                    infoUrl = item.getFullArticleUrl(),
+                    sourceLogoUrl = SOURCE_LOGO_URL,
+                    source = 1
                 )
             }
         } catch (e: Exception) {
@@ -35,12 +39,10 @@ internal class JsonToArtistInfoResolver() :
         return jobj[ARTISTS].asJsonObject
     }
 
-    private fun JsonObject.getArtistName(): String = this[NAME].asString
-
     private fun JsonObject.getInfoContent() =
-            this[BIO]
-                .asJsonObject[CONTENT]
-                .asString.replace("\\n", "\n")
+        this[BIO]
+            .asJsonObject[CONTENT]
+            .asString.replace("\n", "\n")
 
 
     private fun JsonObject.getFullArticleUrl() = this[URL].asString
