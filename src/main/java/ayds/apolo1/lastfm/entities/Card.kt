52825eab18609e285c9063c84b308a0269d9da00
Brunoo886 <42704038+Brunoo886@users.noreply.github.com>
2021-06-16 19:36:38 -0300

    Reformat code + remoción de EmptyCard

--- src/main/java/ayds/apolo1/lastfm/entities/Card.kt
+++ src/main/java/ayds/apolo1/lastfm/entities/Card.kt
@@ -3,7 +3,7 @@ package ayds.apolo1.lastfm.entities
 interface Card {
     val description: String
     val infoUrl: String
-    val sourceLogoUrl :String
+    val sourceLogoUrl: String
     val source: Int
     var isLocallyStored: Boolean
 }
@@ -15,11 +15,3 @@ data class CardImpl(
     override val source: Int,
     override var isLocallyStored: Boolean = false
 ) : Card
-
-object EmptyCard : Card {
-    override val description: String = ""
-    override val infoUrl: String = ""
-    override val sourceLogoUrl: String = ""
-    override val source = -1
-    override var isLocallyStored: Boolean = false
-}
